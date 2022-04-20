7a4d9b9b8200655d74c8e29ad6acde2cd00e4011
bsoelch <benjamin.soelch@googlemail.com>
2022-04-20 18:30:13 +0200

    added a new version of common.h

---
+++ compiler/common.h
@@ -0,0 +1,55 @@
+/*
+ base header file for all compiler concat files
+ */
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include "inttypes.h"
+#include "string.h"
+#include "stdbool.h"
+
+typedef union value_t_Impl value_t;
+
+typedef struct{
+	size_t size;
+	value_t* data;//required capacity can be computed at compile time
+}Stack;
+
+typedef uint64_t type_t;
+typedef double float64_t;
+typedef void(*fptr_t)(Stack*,value_t*/*curried*/);
+typedef void* ptr_t;
+
+union value_t_Impl {
+	//primitive types
+	bool      asBool;
+	uint8_t   asU8;
+	uint32_t  asU32;
+	int64_t   asI64;
+	uint64_t  asU64;
+	float64_t asF64;
+	type_t    asType;
+	//primitive references
+	bool*      asBoolRef;
+	uint8_t*   asU8Ref;
+	uint32_t*  asU32Ref;
+	int64_t*   asI64Ref;
+	uint64_t*  asU64Ref;
+	float64_t* asF64Ref;
+	type_t*    asTypeRef;
+	//pointer types
+	fptr_t    asFPtr;
+	ptr_t     asPtr;
+	//composite types are stored in multiple values
+};
+
+
+void stack_dup(Stack* stack,size_t count);
+void stack_drop(Stack* stack,size_t off,size_t count);
+void stack_rot(Stack* stack,size_t count,size_t steps);
+
+void ptr_call(Stack* stack);
+
+
+#endif /* COMMON_H */
---
+++ compiler/concat.c
@@ -0,0 +1,64 @@
+/*
+  implementations for the functions in common.h
+ */
+
+#include "common.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+void stack_dup(Stack* stack,size_t count){
+	value_t duped=stack->data[stack->size-count];
+	stack->data[stack->size++]=duped;
+}
+void stack_drop(Stack* stack,size_t off,size_t count){
+	memmove(stack->data+stack->size-(off+count),stack->data+stack->size-off,off*sizeof(value_t));
+	stack->size-=count;
+}
+void stack_rot(Stack* stack,size_t count,size_t steps){
+	memmove(stack->data+stack->size      ,stack->data+stack->size-count,steps*sizeof(value_t));
+	memmove(stack->data+stack->size-count,stack->data+stack->size-count+steps,count*sizeof(value_t));
+}
+
+void ptr_call(Stack* stack){
+	stack->size-=2;
+	((fptr_t)(stack->data[stack->size].asPtr))(stack,(value_t*)stack->data[stack->size+1].asPtr);
+}
+
+
+
+//test-functions
+void testFunction(Stack* stack,value_t* curried){
+	printf("called test %p\n",curried);
+	if(curried!=NULL){
+		printf("curried[0]:%"PRIu64"\n",curried[0].asU64);
+	}
+}
+static void printValues(value_t* data,size_t count){
+	fputs("[",stdout);
+	for(size_t i=0;i<count;i++){
+		printf("%"PRIu64", ",data[i].asU64);
+	}
+	puts("..]");
+}
+int main(void){
+	value_t data[100];
+	Stack s={.data=data,.size=0};
+	s.data[s.size++]=(value_t){.asU64=1};
+	s.data[s.size++]=(value_t){.asU64=2};
+	s.data[s.size++]=(value_t){.asU64=3};
+	s.data[s.size++]=(value_t){.asU64=4};
+	printValues(s.data,s.size);
+	stack_dup(&s,1);
+	printValues(s.data,s.size);
+	stack_drop(&s,0,2);
+	printValues(s.data,s.size);
+	stack_dup(&s,2);
+	printValues(s.data,s.size);
+	stack_drop(&s,2,1);
+	printValues(s.data,s.size);
+	stack_rot(&s,3,1);
+	printValues(s.data,s.size);
+	s.data[s.size++]=(value_t){.asFPtr=&testFunction};
+	s.data[s.size++]=(value_t){.asPtr=(value_t[]){(value_t){.asU64=1},(value_t){.asU64=2},(value_t){.asU64=3}}};
+	ptr_call(&s);
+}
